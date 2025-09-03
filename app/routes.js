d367358382466a7a795cc1a9a6da52eba9bcfb99
Iain MacDonald <75545992+IainMacDonaldUX@users.noreply.github.com>
2025-09-03 12:13:25 +0100

    Sprint 5 Q2 25 set up

--- app/routes.js
+++ app/routes.js
@@ -9,6 +9,11 @@ const router = govukPrototypeKit.requests.setupRouter()
 // Add your routes here
 
 //********************
+//* SPRINT 5 - Q2 - 25
+//********************
+require('./routes/sprint5-Q2-25/routes.js')(router);
+
+//********************
 //* SPRINT 2 - Q2 - 25
 //********************
 require('./routes/sprint2-Q2-25/routes.js')(router);
