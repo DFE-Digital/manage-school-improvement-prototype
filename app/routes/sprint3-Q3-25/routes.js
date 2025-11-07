dfa2f3d10ec5d51fdc040cc48e091e114b40a48b
Alexandra Edwards <alexed93@gmail.com>
2025-11-07 11:38:13 +0000

    Tidy supporting org stuff into its own folder. Tidy task list code

--- app/routes/sprint3-Q3-25/routes.js
+++ app/routes/sprint3-Q3-25/routes.js
@@ -136,7 +136,7 @@ router.post('/' + version + '/address-routes-1', function(req, res) {
 
 
  // Run this code when a form is submitted to 'engagement-answer'
- router.post('/' + version + '/task-list/so-answer', function(req, res) {
+ router.post('/' + version + '/choose-preferred-supporting-org/so-answer', function(req, res) {
     
   // Make a variable to give it the value from the checkbox on the index page  
   const supportingOrganisationAnswer = req.body["so-type"];
@@ -144,11 +144,11 @@ router.post('/' + version + '/address-routes-1', function(req, res) {
   // Check whether the variable matches a condition
   if (supportingOrganisationAnswer == "School") {
     // Send user to next page 
-    res.redirect('/' + version + '/task-list/choose-supporting-organisation-school')
+    res.redirect('/' + version + '/choose-preferred-supporting-org/choose-supporting-organisation-school')
   }   
   else { 
     // Send user to next page 
-    res.redirect('/' + version + '/task-list/choose-supporting-organisation-trust')
+    res.redirect('/' + version + '/choose-preferred-supporting-org/choose-supporting-organisation-trust')
   }
 })
 
