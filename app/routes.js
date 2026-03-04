ec16732c72fc04e96819b8184f81efd978d50f86
Alexandra Edwards <alexed93@gmail.com>
2026-03-04 14:06:18 +0000

    Sprint setup

--- app/routes.js
+++ app/routes.js
@@ -9,6 +9,11 @@ const router = govukPrototypeKit.requests.setupRouter()
 // Add your routes here
 
 //********************
+//* SPRINT 5 - Q4 - 26
+//********************
+require('./routes/sprint5-Q4-26/routes.js')(router);
+
+//********************
 //* SPRINT 3 - Q4 - 26
 //********************
 require('./routes/sprint3-Q4-26/routes.js')(router);
